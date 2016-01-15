6d20c3f5e9001e66550d24f7a401b5363ab212a9
Duncan Bayne <duncan@cogent.co>
2016-01-16 08:23:30 +1100

    Add Sublime PPA without prompting

--- install_sublime.sh
+++ install_sublime.sh
@@ -3,6 +3,6 @@ set -e
 
 echo Installing Sublime Text 3
 
-add-apt-repository ppa:webupd8team/sublime-text-3
+add-apt-repository -y ppa:webupd8team/sublime-text-3
 apt-get update
 apt-get install sublime-text-installer
